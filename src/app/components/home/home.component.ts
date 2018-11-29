26e78c2e4f3fde2ab97df3903e8b3a020e2fa8aa
Nicolas Kornberg <nicokorn96@gmail.com>
2018-11-29 19:57:40 -0300

    cambios finales

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,10 +10,19 @@ export class HomeComponent {
 
   nuevasCanciones: any[] = [];
   loading: boolean;
+
+  error: boolean;
+  mensajeError: string;
+
   constructor( private spotify: SpotifyService) {
     this.loading = true;
+    this.error = false;
     spotify.getNewReleases().subscribe( (data: any) => {
       this.nuevasCanciones = data;
+      this.loading = false;
+    }, ( errorServicio ) => {
+      this.error = true;
+      this.mensajeError = errorServicio.error.error.message;
       this.loading = false;
     });
    }
